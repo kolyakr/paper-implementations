41965fba58c76c8bab4004bb9d9a701eb37c06f1
kolyakr <krivoruchko.kolya@gmail.com>
2026-02-03 23:16:15 +0200

    replace paper

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,2 @@
-from .utils import count_params, get_gpu, get_loss_function, get_optimizer, min_max_scaling
+from .utils import count_params, get_gpu, get_loss_function, \
+                   get_optimizer, min_max_scaling
